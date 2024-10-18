721f13188abb03e3525fa6ce42cf7341991eb4c9
EmilianoTL <atorresl1900@gmail.com>
2024-10-17 23:07:05 -0600

    Clase de conjunto de conjunto de estados con su metodo para comparar dos objetos de la clase

---
+++ src/EstadosSi.ts
@@ -0,0 +1,31 @@
+import { AFN } from './AFN';
+import { Estado } from './Estado';
+
+class Si {
+    id: number;                // Identificador
+    S: Set<Estado>;           // Conjunto de estados
+
+    constructor(i: number = 0, S: Set<Estado> = new Set()) {
+        this.id = i;
+        this.S = S;
+    }
+    Equals(obj: Si): boolean {
+        if (this.S.size !== obj.S.size) return false;
+
+        const inObj = new Set((this.S.size < obj.S.size) ?
+            [...this.S].filter(x => !obj.S.has(x)) : [...obj.S].filter(x => !this.S.has(x))
+        );
+        return inObj.size === 0;
+    }
+}
+/*
+// Función IndiceCaracter para obtener el índice del carácter en el alfabeto
+function IndiceCaracter(alfabeto: string[], c: string): number {
+    return alfabeto.indexOf(c);
+}
+
+// Implementación de la función EstadosSi
+function EstadosSi(): void {
+
+}*/
+export { Si };
